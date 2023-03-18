bf8f8652601271435615c6f17dbffad2db6e9b0d
OhHyeonJu0415 <guswn2209@gmail.com>
2023-03-18 10:38:41 +0900

    feat : 레시피 목록 요청 및 오류 핸들링 구현

--- scripts/section/FoodContainer.js
+++ scripts/section/FoodContainer.js
@@ -1,43 +1,71 @@
 import Ingredients from "../component/Food/Ingredients.js";
 import { setPath } from "../component/Food/SetPath.js";
+import { getData } from "../component/GetData.js";
 
 class FoodContainer {
-  constructor($container, $data, $year, $month) {
+  constructor($container, $foodData, $year, $month) {
     this.$container = $container;
-    this.$data = $data;
+    this.$foodData = $foodData;
+    this.$recipeData = "";
     this.$year = $year;
     this.$month = $month;
     this.path = 0;
   }
 
   render() {
-    const ingredients = new Ingredients();
-    const path = setPath();
-    const root = document.querySelector("#foodContainer");
-    const line = document.getElementsByClassName("boldLine");
-    const date = document.getElementById("foodDate");
-    root.classList.remove("disPlayNone");
-    line[0].classList.remove("disPlayNone");
-    window.scrollTo({ left: 0, top: screen.height, behavior: "smooth" });
+    (async () => {
+      let url = `https://cors-anywhere.herokuapp.com/http://api.nongsaro.go.kr/service/monthFd/monthNewFdLst?apiKey=202303061MOHKXNQUQY72RTDRDBJGA&thisYear=${this.$year}&thisMonth=${this.$month}`;
+      try {
+        const ret = await getData("GET", url);
 
-    //타이틀
-    date.innerText = `${this.$year}년 ${this.$month}월`;
+        //조회 정보 있음
+        this.$recipeData = ret;
+        const ingredients = new Ingredients();
 
-    //첫번째 음식 사진으로 초기화
-    ingredients.render(path[this.path].image, path[this.path].title);
+        //이미지 경로 및 제목 가공
+        const path = setPath();
+        const root = document.querySelector("#foodContainer");
+        const line = document.getElementsByClassName("boldLine");
+        const date = document.getElementById("foodDate");
 
-    const leftArrow = document.getElementById("leftArrow");
-    const rightArrow = document.getElementById("rightArrow");
+        //푸드페이지쪽으로 이동
+        root.classList.remove("disPlayNone");
+        line[0].classList.remove("disPlayNone");
+        window.scrollTo({ left: 0, top: screen.height, behavior: "smooth" });
 
-    leftArrow.addEventListener("click", () => {
-      if (this.path > 0) this.path = this.path - 1;
-      ingredients.render(path[this.path].image, path[this.path].title);
-    });
+        //타이틀
+        date.innerText = `${this.$year}년 ${this.$month}월`;
 
-    rightArrow.addEventListener("click", () => {
-      if (this.path < path.length - 1) this.path = this.path + 1;
-      ingredients.render(path[this.path].image, path[this.path].title);
-    });
+        //첫번째 음식 사진으로 초기화
+        ingredients.render(path[this.path].image, path[this.path].title);
+
+        const leftArrow = document.getElementById("leftArrow");
+        const rightArrow = document.getElementById("rightArrow");
+
+        //이전 식재료 렌더링
+        leftArrow.addEventListener("click", () => {
+          if (this.path > 0) this.path = this.path - 1;
+          ingredients.render(path[this.path].image, path[this.path].title);
+        });
+
+        //다음 식재료 렌더링
+        rightArrow.addEventListener("click", () => {
+          if (this.path < path.length - 1) this.path = this.path + 1;
+          ingredients.render(path[this.path].image, path[this.path].title);
+        });
+
+        if (ret === undefined || ret === null) {
+          //레시피 조회 정보 없음
+          const noRecipe = document.getElementById("alertRecipe");
+          noRecipe.innerText = `${this.$year}년 ${this.$month}월에는 조회 가능한 레시피가 없습니다.`;
+        }
+      } catch (error) {
+        console.log(error);
+        alert(
+          "예상치 못한 에러가 발생했습니다! 상단 메일 버튼을 통해 개발자에게 제보해주세요 😭"
+        );
+      }
+    })();
   }
 }
 
